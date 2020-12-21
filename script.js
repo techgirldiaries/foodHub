d303d87dba3a85f3d857d76c35f0256b8e649c04
Oluwakemi <talktechwithkemi@gmail.com>
2020-12-21 05:08:35 +0100

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+    const menu = document.querySelector('.menuToggle');
+    const nav = document.querySelector('.nav');
+    menuToggle.classList.toggle("active");
+    nav.classList.toggle('active');
+}
